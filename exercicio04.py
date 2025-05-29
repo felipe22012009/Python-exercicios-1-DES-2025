6da112b5f5ee793345e402b7f22e72e13ec2e859
felipe22012009 <fernando.silva.ribeiro@escola.pr.gov.br>
2025-05-29 14:45:59 +0000

    exercicio4 terminado

--- exercicio04.py
+++ exercicio04.py
@@ -1,7 +1,3 @@
-# Isabela está desenvolvendo um aplicativo de corrida que calcula a velocidade média do usuário.
-# O programa deve receber a distância percorrida e o tempo gasto, calcular a velocidade e indicar se foi 
-# lenta (<5 km/h), moderada (5 a 10 km/h) ou rápida (>10 km/h).
-
 peso = float(input("Digite o peso (kg): "))
 altura = float(input("Digite a altura (m): "))
 
