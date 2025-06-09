4c6335c318900d947745568779eee2d0c798241c
felipe22012009 <fernando.silva.ribeiro@escola.pr.gov.br>
2025-06-09 13:42:24 +0000

    curso01.py terminado

--- exercicio010.py
+++ exercicio010.py
@@ -3,5 +3,8 @@ parcela_maxima = salario _mês * 0.35
 
 if salario_mês < 3000:
    print("renata tem salario superior a R$ 3.000,00")
-   print("a parcela maxima que ela pode ter é: R$"{})
+   print("a parcela maxima que ela pode ter é: R$"{parcela_maxima:2f}")
+else:
+   print("O salario do mês de renata é inferior a R$3.000,00.")
+   print("Ela não entende   ")
 
