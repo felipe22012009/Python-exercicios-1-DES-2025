bc3bd4d44b8e721cf5a2d465bd62df0a06aafe9c
felipe22012009 <fernando.silva.ribeiro@escola.pr.gov.br>
2025-06-10 12:19:57 +0000

    vetormatriz.py

---
+++ vetormatriz.py
@@ -0,0 +1,18 @@
+aluno = ["alice", "bruno", "carla",]
+dias = ["seg", "ter", "qua", "qui",]
+
+reserva = [{"ausente" for _ in dias} for _ in alunos]
+
+print("preencha com 'S' para presença e 'X' para ausência:")
+
+for i, aluno in enumerate(aluno) :
+    print(f"\nAluno: {aluno}")
+    for j, dia in enumerate(dias):
+        entrada = input(f" {dia}: ")
+        if entrada.upper() == 'S':
+            reservas[i][j] = "presentes"
+
+print("\nTabela de reservas:")
+print(f"{'Aluno'} {' '.join(f'{d:<10}' for d in dias)}")
+for i, aluno in enumerate(alunos):
+    print(f"{aluno:<10} {' '.join([f'{res:<10}' for res in reservas[i]])}")
