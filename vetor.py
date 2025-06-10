4f48a240ccef601a7719d1c6e47f15f7d2452b59
felipe22012009 <fernando.silva.ribeiro@escola.pr.gov.br>
2025-06-10 11:42:17 +0000

    vetor.py

---
+++ vetor.py
@@ -0,0 +1,14 @@
+# lista (vetor) com 5 frutas
+frutas = ["Maçã", "Banana", "Laranja", "Uva","Manga"]
+print("Lista de frutas:")
+for i in range(len(frutas)):
+    print(f"{i +1}") - {frutas[i]}
+    posição_sorteada = random.redint9=(1, 5)
+    if palpite = input("qual frita você acha esta na posição")
+fruta_certa = frutas[posição_sorteada -1]
+if palpite.capitalize() == fruta.certa:
+    print(" parabéns! você acertou!")
+    print(f"A fruta na posição [posição_sorteda]" era realmente {fruta_certa}.")
+else:
+    print(" que pena, você errou.")
+    print(f"A fruta na posição{posição_sorteada} era {fruta_certa}.")
