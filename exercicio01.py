8a57e749c9b1190914addcbca9e395631201d562
felipe22012009 <fernando.silva.ribeiro@escola.pr.gov.br>
2025-06-10 11:42:02 +0000

    vetor.py

--- exercicio01.py
+++ exercicio01.py
@@ -1,6 +1,7 @@
-# Gabriel está acompanhando o desempenho de dois cursos online que lançou: Python Básico e JavaScript Essencial.
-# Ele quer saber qual curso teve mais avaliações no último mês.
-
-# Crie um programa que receba o número de avaliações de cada curso e exiba qual teve mais.
-# Caso as quantidades sejam iguais, exiba uma mensagem dizendo que houve empate.
+curso01 = int(input("digite quantas avaliações teve no curso 01: "))
+curso02 = int(input("digite quantas aviliações teve no curso 02: "))
+if curso01 == curso02
+print("o curso01 e maior: ")
+else:
+print("o cuirso02 e maior: ")
 
