320f0c7e11eea9226adddc2a9da98676ef8f0aee
felipe22012009 <fernando.silva.ribeiro@escola.pr.gov.br>
2025-06-12 11:58:18 +0000

    dado.py terminado

--- dado.py
+++ dado.py
@@ -4,4 +4,7 @@ input("Presione o enter para lançar o dado")
 
 resultado = random.randint(1,6)
 
-print(f"O dado rolou :")S
+print(f"O dado rolou : {resultado}");
+
+if resultado == 6 :
+print("Parabéns, você conseguiu")
--- exercicio03.py
+++ exercicio03.py
@@ -1,3 +1,8 @@
 # Rafael trabalha com armazenamento de grãos e precisa garantir que a umidade do ar no local não ultrapasse 70%.
 # Escreva um programa que receba o valor da umidade atual e exiba um alerta se estiver acima do limite.
-
+umidadeMax = int(input("Digite a porcentagem da umidade do local:"))
+if umidadeMAX >=70:
+   print("ALERTA: Umidade muito alta!!!")
+else:
+   print("Nada a se preocupar")
+   
