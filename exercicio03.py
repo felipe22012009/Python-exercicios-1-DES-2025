ad561bc2ce70375cc53a91d58e3c2896381160fb
felipe22012009 <fernando.silva.ribeiro@escola.pr.gov.br>
2025-06-12 12:05:56 +0000

    exercicio03.py enviar

--- exercicio03.py
+++ exercicio03.py
@@ -1,6 +1,4 @@
-# Rafael trabalha com armazenamento de grãos e precisa garantir que a umidade do ar no local não ultrapasse 70%.
-# Escreva um programa que receba o valor da umidade atual e exiba um alerta se estiver acima do limite.
-umidadeMax = int(input("Digite a porcentagem da umidade do local:"))
+umidadeMAX = int(input("Digite a porcentagem da umidade do local:"))
 if umidadeMAX >=70:
    print("ALERTA: Umidade muito alta!!!")
 else:
