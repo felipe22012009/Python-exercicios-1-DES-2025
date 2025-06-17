3436f78a8eb2ae9674e1baecd4dbfa7fb6d0bd88
felipe22012009 <fernando.silva.ribeiro@escola.pr.gov.br>
2025-06-17 13:42:52 +0000

    cart

---
+++ carta02.py
@@ -0,0 +1,6 @@
+email_remetente = input("Digite o nome do email do remetente: ")
+mensagem = input("Digite sua mensagem: ")
+email_destinatario = input("Digite seu email: ")
+
+print("\n--- Seu email ---")
+print(f"Olá {email_destinatario}, \n\n{mensagem}\n\nAtenciosamente, \n{email_remetente}") 
